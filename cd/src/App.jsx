1298accf1a2ff4b769607d9af12c192cc394dacf
avermx <aniket125verma@gmail.com>
2025-03-29 14:17:48 +0530

    init

--- cd/src/App.jsx
+++ cd/src/App.jsx
@@ -6,22 +6,20 @@ import './App.css'
 
 function App() {
   const [edata, setedata] = useState([])
-  const [pokedata, setpokedata] = useState([])
 
-
-
-
-
-    
- 
-
-      
-  
-  
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`).then((res)=>res.json()).then((res)=>(setedata(res)))
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+      .then((res) => res.json())
+      .then((data) => (
+        data.results.forEach((e) => {
+          fetch(e.url).then((res) => (res.json()))
+          .then((data) => (setedata((e)=>[...e,data])))
+        })
+      ))
 
   }, [])
+
+
 
 
   return (
@@ -78,58 +76,52 @@ function App() {
       </div>
       <div className='w-full  bg-rose-100 px-[7%] py-[2%] flex gap-5 flex-wrap justify-center'>
         {
-          edata?.results?.map((anime,index)=>(
-            <div className='block h-[50%] w-[25%] bg-[url(/half.svg)] bg-[rgba(6,14,32,0.8)] bg-no-repeat bg-contain rounded-4xl'>
-            <div className='card-poke w-full'>
-              <div className='card-poke-img flex justify-center '>
-                <img className='w-[60%]' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-              </div>
-              <div className='flex justify-center flex-col gap-2 text-white'>
-                <div className='flex justify-center text-3xl font-semibold'>
-                  <h1>{anime.name}</h1>
+          edata?.map((anime, index) => (
+            <div key={index} className='block h-[50%] w-[25%] bg-[url(/half.svg)] bg-[rgba(6,14,32,0.8)] bg-no-repeat bg-contain rounded-4xl'>
+              <div className='card-poke w-full'>
+                <div className='card-poke-img flex justify-center '>
+                  <img className='w-[60%]' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
                 </div>
-                <div className='flex justify-center gap-5 text-[0.95rem] font-bold'>
-                  <span className='bg-[#66CD32] p-[0.5%] w-[24%] flex justify-center rounded-[5px]'>
-                    GRASS
-                  </span>
-                  <span className='bg-[#AA66CC] p-[0.5%] w-[25%] flex justify-center rounded-[5px]'>
-                    POISON
-                  </span>
-                </div>
-                <div className='flex justify-center gap-24'>
-                  <span>
-                    0.7M
-                  </span>
-                  <span>
-                    6.9KG
-                  </span>
-                </div>
-                <div className='flex justify-center gap-8 mt-[-2%] '>
-                  <div className='flex gap-1 '>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M229.66,90.34,90.34,229.66a8,8,0,0,1-11.31,0L26.34,177a8,8,0,0,1,0-11.31L165.66,26.34a8,8,0,0,1,11.31,0L229.66,79A8,8,0,0,1,229.66,90.34Z" opacity="0.2"></path><path d="M235.32,73.37,182.63,20.69a16,16,0,0,0-22.63,0L20.68,160a16,16,0,0,0,0,22.63l52.69,52.68a16,16,0,0,0,22.63,0L235.32,96A16,16,0,0,0,235.32,73.37ZM84.68,224,32,171.31l32-32,26.34,26.35a8,8,0,0,0,11.32-11.32L75.31,128,96,107.31l26.34,26.35a8,8,0,0,0,11.32-11.32L107.31,96,128,75.31l26.34,26.35a8,8,0,0,0,11.32-11.32L139.31,64l32-32L224,84.69Z"></path></svg>
-                    <span >
-                      Height
+                <div className='flex justify-center flex-col gap-2 text-white'>
+                  <div className='flex justify-center text-3xl font-semibold'>
+                    <h1>{anime.name}</h1>
+                  </div>
+                  <div className='flex justify-center gap-5 text-[0.95rem] font-bold'>
+                    <span className='bg-[#66CD32] p-[0.5%] w-[24%] flex justify-center rounded-[5px]'>
+                      GRASS
+                    </span>
+                    <span className='bg-[#AA66CC] p-[0.5%] w-[25%] flex justify-center rounded-[5px]'>
+                      POISON
                     </span>
                   </div>
-                  <div className='flex gap-1'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M96,64V192a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V64a8,8,0,0,1,8-8H88A8,8,0,0,1,96,64Zm96-8H168a8,8,0,0,0-8,8V192a8,8,0,0,0,8,8h24a8,8,0,0,0,8-8V64A8,8,0,0,0,192,56Z" opacity="0.2"></path><path d="M248,120h-8V88a16,16,0,0,0-16-16H208V64a16,16,0,0,0-16-16H168a16,16,0,0,0-16,16v56H104V64A16,16,0,0,0,88,48H64A16,16,0,0,0,48,64v8H32A16,16,0,0,0,16,88v32H8a8,8,0,0,0,0,16h8v32a16,16,0,0,0,16,16H48v8a16,16,0,0,0,16,16H88a16,16,0,0,0,16-16V136h48v56a16,16,0,0,0,16,16h24a16,16,0,0,0,16-16v-8h16a16,16,0,0,0,16-16V136h8a8,8,0,0,0,0-16ZM32,168V88H48v80Zm56,24H64V64H88V192Zm104,0H168V64h24V175.83c0,.06,0,.11,0,.17s0,.12,0,.17V192Zm32-24H208V88h16Z"></path></svg>
+                  <div className='flex justify-center gap-24'>
                     <span>
-                      Weight
+                      0.7M
+                    </span>
+                    <span>
+                      6.9KG
                     </span>
                   </div>
+                  <div className='flex justify-center gap-8 mt-[-2%] '>
+                    <div className='flex gap-1 '>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M229.66,90.34,90.34,229.66a8,8,0,0,1-11.31,0L26.34,177a8,8,0,0,1,0-11.31L165.66,26.34a8,8,0,0,1,11.31,0L229.66,79A8,8,0,0,1,229.66,90.34Z" opacity="0.2"></path><path d="M235.32,73.37,182.63,20.69a16,16,0,0,0-22.63,0L20.68,160a16,16,0,0,0,0,22.63l52.69,52.68a16,16,0,0,0,22.63,0L235.32,96A16,16,0,0,0,235.32,73.37ZM84.68,224,32,171.31l32-32,26.34,26.35a8,8,0,0,0,11.32-11.32L75.31,128,96,107.31l26.34,26.35a8,8,0,0,0,11.32-11.32L107.31,96,128,75.31l26.34,26.35a8,8,0,0,0,11.32-11.32L139.31,64l32-32L224,84.69Z"></path></svg>
+                      <span >
+                        Height
+                      </span>
+                    </div>
+                    <div className='flex gap-1'>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M96,64V192a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V64a8,8,0,0,1,8-8H88A8,8,0,0,1,96,64Zm96-8H168a8,8,0,0,0-8,8V192a8,8,0,0,0,8,8h24a8,8,0,0,0,8-8V64A8,8,0,0,0,192,56Z" opacity="0.2"></path><path d="M248,120h-8V88a16,16,0,0,0-16-16H208V64a16,16,0,0,0-16-16H168a16,16,0,0,0-16,16v56H104V64A16,16,0,0,0,88,48H64A16,16,0,0,0,48,64v8H32A16,16,0,0,0,16,88v32H8a8,8,0,0,0,0,16h8v32a16,16,0,0,0,16,16H48v8a16,16,0,0,0,16,16H88a16,16,0,0,0,16-16V136h48v56a16,16,0,0,0,16,16h24a16,16,0,0,0,16-16v-8h16a16,16,0,0,0,16-16V136h8a8,8,0,0,0,0-16ZM32,168V88H48v80Zm56,24H64V64H88V192Zm104,0H168V64h24V175.83c0,.06,0,.11,0,.17s0,.12,0,.17V192Zm32-24H208V88h16Z"></path></svg>
+                      <span>
+                        Weight
+                      </span>
+                    </div>
+                  </div>
                 </div>
+
               </div>
-  
             </div>
-          </div>
           ))
         }
-
-
-
-
-
-
       </div>
     </>
   )
