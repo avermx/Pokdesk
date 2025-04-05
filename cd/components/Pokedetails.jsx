cc8a05a3efb6e41d84e741e1c7b41ffa4f638d1e
avermx <aniket125verma@gmail.com>
2025-04-05 15:55:53 +0530

    init

--- cd/components/Pokedetails.jsx
+++ cd/components/Pokedetails.jsx
@@ -22,18 +22,28 @@ const Pokedetails = () => {
   useEffect(() => {
     FetchData();
   }, [])
+
+Pokedata.forEach((e)=>{
+  console.log(e)
+  
+})
+
   return (
     <>
       <div className='bg-amber-900 w-full h-screen'>
         <div className=' w-full text-white flex justify-center'>
-          <div className='h-[55vh] w-[25%] flex justify-center'>
+          <div className='h-[55vh] w-[25%] flex justify-center '>
             <img className='' src={Pokedata.sprites?.other?.['official-artwork'].front_default} />
           </div>
         </div>
-        <div className='bg-black h-[45%] w-full text-white flex justify-center flex-col '>
+        <div className=' h-[45%] w-full text-white flex justify-center flex-col capitalize'>
           <h1 className='text-4xl flex justify-center'>{Pokedata.name}</h1>
-          <div className='w-full h-full bg-amber-100'>
-
+          <div className='w-full h-full bg-amber-100 p-5 flex flex-col gap-2 justify-center items-center'>
+            <div className='h-[10%] w-[80%] bg-white rounded-4xl'>
+                        <div className='h-full w-[50%] bg-black rounded-full'>
+                        {/* {Pokedata.stats[0].base_stat} */}
+                        </div>
+                  </div>
           </div>
         </div>
       </div>
