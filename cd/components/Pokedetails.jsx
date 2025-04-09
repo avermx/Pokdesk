ca661a7f6b5a98d6bebf7945affae87bfc37c533
avermx <aniket125verma@gmail.com>
2025-04-09 21:21:12 +0530

    minor changes

--- cd/components/Pokedetails.jsx
+++ cd/components/Pokedetails.jsx
@@ -23,27 +23,27 @@ const Pokedetails = () => {
     FetchData();
   }, [])
 
-Pokedata.forEach((e)=>{
-  console.log(e)
-  
-})
 
   return (
     <>
-      <div className='bg-amber-900 w-full h-screen'>
+      <div className=' w-full  bg-red-100'>
         <div className=' w-full text-white flex justify-center'>
           <div className='h-[55vh] w-[25%] flex justify-center '>
             <img className='' src={Pokedata.sprites?.other?.['official-artwork'].front_default} />
           </div>
         </div>
-        <div className=' h-[45%] w-full text-white flex justify-center flex-col capitalize'>
+        <div className=' h-[45%] w-full text-white flex justify-center flex-col capitalize gap-5'>
           <h1 className='text-4xl flex justify-center'>{Pokedata.name}</h1>
-          <div className='w-full h-full bg-amber-100 p-5 flex flex-col gap-2 justify-center items-center'>
-            <div className='h-[10%] w-[80%] bg-white rounded-4xl'>
-                        <div className='h-full w-[50%] bg-black rounded-full'>
-                        {/* {Pokedata.stats[0].base_stat} */}
-                        </div>
-                  </div>
+          <div className=' bg-amber-100 p-1 flex flex-col gap-2 justify-center items-center p-5'>
+            {Pokedata?.stats?.map((poke) => (
+              <div className='h-[20%] w-[60vw] bg-white rounded-4xl '>
+                <div className=' bg-black rounded-full text-[0.9rem] text-center content-center' style={{ width: `${poke.base_stat}vh` }} >
+                  {poke.stat.name}
+                </div>
+              </div>
+
+            ))}
+            
           </div>
         </div>
       </div>
