fe84382aaaf14b2978453ca461f9c1d04843be39
avermx <aniket125verma@gmail.com>
2025-04-27 20:42:21 +0530

    added evolution of Pokemon

--- cd/components/Evolution.jsx
+++ cd/components/Evolution.jsx
@@ -1,25 +1,9 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import { FaArrowRight } from "react-icons/fa6";
 const Evolution = ({ speciesUrl }) => {
   const [evolution, setEvolution] = useState();
   const [evolution2, setEvolution2] = useState([]);
-
-  const API = `https://pokeapi.co/api/v2/pokemon/pikachu`
-  const FetchPoke = async () => {
-    try {
-      const res = await fetch(API)
-      const data = await res.json()
-      console.log(data)
-
-    } catch (error) {
-      console.log({ error });
-    }
-  }
-
-  useEffect(() => {
-    FetchPoke()
-  }, [])
-
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     fetch(speciesUrl?.url)
@@ -41,43 +25,56 @@ const Evolution = ({ speciesUrl }) => {
         ? evolution?.evolves_to[0]?.evolves_to[0]?.species?.name
         : "",
     ]);
-
-    evolution2.forEach((e) => {})
-
-
-
-
-    // setEvolution2((pre)=>[...pre,evolution?.evolves_to[0]?.species?.name])
-    // setEvolution2((pre)=>[...pre,evolution?.evolves_to[0]?.evolves_to[0]?.species?.name])
-
-    // setEvolution2((e)=>[...e,evolution?.evolves_to[0]?.species?.name])
-
-    // if(evolution?.species?.name){
-    //   setEvolution2((PreviousState)=>[...PreviousState,evolution?.species?.name])
-    // }
-    // if(evolution?.evolves_to[0]?.species?.name){
-    //   setEvolution2((PreviousState)=>[...PreviousState,evolution?.evolves_to[0]?.species?.name])
-    // }
-    // if(evolution?.evolves_to[0]?.evolves_to[0]?.species?.name){
-    //   setEvolution2((PreviousState)=>[...PreviousState,evolution?.evolves_to[0]?.evolves_to[0]?.species?.name])
-    // }
-
-    // setEvolution2((PreviousState)=>[...PreviousState,evolution?.evolves_to[0]?.species?.name])
-    // setEvolution2((PreviousState)=>[...PreviousState,evolution?.evolves_to[0]?.evolves_to[0].species?.name])
   }, [evolution]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const imgs = await Promise.all(
+        evolution2.map(async (e) => {
+          if (e !== "") {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${e}`);
+            const data = await res.json();
+            return data.sprites.other["official-artwork"].front_default;
+          } else {
+            return null;
+          }
+        })
+      );
+      setImages(imgs); // update state
+    };
+
+    if (evolution2.length > 0) {
+      fetchImages();
+    }
+  }, [evolution2]);
 
   return (
     <>
       <div className=" justify-center flex">
-        <div className="bg-amber-200 h-[30vh] w-[50%] p-[1%]">
-          <h1>Evolution Chain</h1>
-          <div className="py-2 ">
-            <div className="flex gap-5">{evolution2.map((e) => (
-              <>
-                <p>{e}</p>
+        <div className=" h-[30vh] w-[50%] p-[1%]">
+          <h1 className="font-bold">Evolution Chain</h1>
+          <div className="py-2 w-full  ">
+            <div className="flex gap-5 items-center justify-evenly w-full ">
+              {evolution2[0] != ""
+                ? evolution2.filter(name => name !== '').map(
+                    (e, i,arr) =>
+                      e != "" && (
+                        <>
+                          <div key={i} className=" flex flex-col items-center">
+                            <img
+                              src={images[i]}
+                              alt={e}
+                              className="h-[80%] w-40 "
+                            />
 
-              </>
-            ))}</div>
+                            <p className="text-center">{e}</p>
+                            </div>
+                            {i !== arr.length - 1 && <FaArrowRight size={30}/>}
+                        </>
+                      )
+                  )
+                : ""}
+            </div>
           </div>
         </div>
       </div>
